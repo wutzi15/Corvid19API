1f76072071fd3838f3865ba568a1d7a19cfb58c6
Jan-Erik Matthies <Jan-Erik Matthies@DESKTOP-R5V6C0C.lan>
2020-03-21 11:23:11 +0100

    insert area

--- python/api/area.py
+++ python/api/area.py
@@ -2,11 +2,17 @@ from DBConnection import DBConnection
 
 def read_all():
     return Area().loadAll();
+def add(area):
+    return Area().add(area);
 
 class Area(object):
     def loadAll(self):
         statisticDB = DBConnection.getStatisticDB()
         return self.readCursor(statisticDB["Area"].find({}))
+    
+    def add(self, area):
+        statisticDB = DBConnection.getStatisticDB()
+        statisticDB["Area"].insert(area);
     
     def readCursor(self, cursor):
         areas = list()

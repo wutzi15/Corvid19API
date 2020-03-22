abe5f7c1dfc6ce0b73c9a4dc0f807e8aced84c8e
Jan-Erik Matthies <jem@jemtech.de>
2020-03-22 15:40:31 +0100

    population api

---
+++ python/api/population.py
@@ -0,0 +1,29 @@
+from DBConnection import DBConnection
+
+def read_all():
+    return Population().loadAll()
+def search(population):
+    return Population().search(population)
+
+class Population(object):
+    def loadAll(self):
+        statisticDB = DBConnection.getStatisticDB()
+        return self.readCursor(statisticDB["Population"].find({}))
+    
+    def search(self, measures):
+        statisticDB = DBConnection.getStatisticDB()
+        return self.readCursor(statisticDB["Population"].find(population))
+    
+    
+    def readCursor(self, cursor):
+        populations = list()
+        for population in cursor:
+            population = self.dBToApi(population)
+            populations.append(population)
+        return populations
+    
+    def dBToApi(self, population):
+        if (population is None) :
+            return None
+        population["_id"] = str(population["_id"])
+        return population
--- python/api/unPopuPipe.py
+++ python/api/unPopuPipe.py
@@ -18,14 +18,76 @@ for pops in popDatas:
         popDb["adm"][0] = pops["Location"]
     popDb["VarID"] = pops["VarID"]
     popDb["Variant"] = pops["Variant"]
-    popDb["Time"] = datetime.datetime.strptime(pops["Time"], '%Y')
-    popDb["MidPeriod"] = float(pops["MidPeriod"])
-    agestart = int(pops["AgeGrpStart"])
-    ageSpan = int(pops["AgeGrpSpan"])
-    ageEnd = agestart + ageSpan -1
-    popDb["ageRange"] = {"lower": agestart, "upper": ageEnd}
-    popDb["PopMale"] = float(pops["PopMale"])
-    popDb["PopFemale"] = float(pops["PopFemale"])
-    popDb["PopTotal"] = float(pops["PopTotal"])
+    try:
+        popDb["Time"] = datetime.datetime.strptime(pops["Time"], '%Y')
+    except:
+        popDb["Time"] = "NaN"
+    try:
+        popDb["MidPeriod"] = float(pops["MidPeriod"])
+    except:
+        popDb["MidPeriod"] = "NaN"
+    try:
+        agestart = int(pops["AgeGrpStart"])
+        ageSpan = int(pops["AgeGrpSpan"])
+        ageEnd = agestart + ageSpan -1
+        popDb["ageRange"] = {"lower": agestart, "upper": ageEnd}
+    except:
+        popDb["ageRange"] = "NaN"
+    try:
+        popDb["PopMale"] = int(pops["PopMale"].replace('.', ''))
+    except:
+        popDb["PopMale"] = "NaN"
+    try:
+        popDb["PopFemale"] = int(pops["PopFemale"].replace('.', ''))
+    except:
+        popDb["PopFemale"] = "NaN"
+    try:
+        popDb["PopTotal"] = int(pops["PopTotal"].replace('.', ''))
+    except:
+        popDb["PopTotal"] = "NaN"
+    popDb["PopDensity"] = "NaN"
+    popDb["source"] = dataSource
+    statistic["Population"].insert(popDb)
+    
+
+dataSource = source.getSource({"name": "UN TotalPopulationBySex"})
+rawDB = connection["raw"]
+statistic = DBConnection.getStatisticDB();
+popDatas = rawDB["unitedNations_TotalPopulationBySex"].find({})
+for pops in popDatas:
+    popDb = dict()
+    popDb["LocID"] = pops["LocID"]
+    popDb["adm"] = ["","",""]
+    try:
+        popDb["adm"][0] = pycountry.countries.get(name=pops["Location"]).alpha_2
+    except:
+        popDb["adm"][0] = pops["Location"]
+    popDb["VarID"] = pops["VarID"]
+    popDb["Variant"] = pops["Variant"]
+    try:
+        popDb["Time"] = datetime.datetime.strptime(pops["Time"], '%Y')
+    except:
+        popDb["Time"] = "NaN"
+    try:
+        popDb["MidPeriod"] = float(pops["MidPeriod"])
+    except:
+        popDb["MidPeriod"] = "NaN"
+    popDb["ageRange"] = "NaN"
+    try:
+        popDb["PopMale"] = int(pops["PopMale"].replace('.', ''))
+    except:
+        popDb["PopMale"] = "NaN"
+    try:
+        popDb["PopFemale"] = int(pops["PopFemale"].replace('.', ''))
+    except:
+        popDb["PopFemale"] = "NaN"
+    try:
+        popDb["PopTotal"] = int(pops["PopTotal"].replace('.', ''))
+    except:
+        popDb["PopTotal"] = "NaN"
+    try:
+        popDb["PopDensity"] = int(pops["PopDensity"].replace('.', ''))
+    except:
+        popDb["PopDensity"] = "NaN"
     popDb["source"] = dataSource
     statistic["Population"].insert(popDb)

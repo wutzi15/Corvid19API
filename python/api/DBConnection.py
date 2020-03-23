2f897a8db38b0efddd2a2c4057bc3276d9765f73
Jan-Erik Matthies <jem@jemtech.de>
2020-03-23 08:54:14 +0100

    docu

--- python/api/DBConnection.py
+++ python/api/DBConnection.py
@@ -1,13 +1,18 @@
+#---------------------------
+# handles transparent the connection to the MongoDB
+#---------------------------
 import pymongo
 
 class DBConnection:
   USER_NAME = "root"
   PASSWORD = "challenge1757"
   
+  # returns the connection to the DB server
   @staticmethod
   def getConnection():
     return pymongo.MongoClient("mongodb://" + DBConnection.USER_NAME + ":" + DBConnection.PASSWORD + "@bene.gridpiloten.de:27017/")
   
+  # returns the connection the live database
   @staticmethod  
   def getStatisticDB():
     return DBConnection.getConnection()["Statistic"]

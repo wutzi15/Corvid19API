ca45b349edbe358c603031d16f99d11e33b9dfcc
Jan-Erik Matthies <jem@jemtech.de>
2020-03-23 08:27:56 +0100

    cases docu

--- python/api/cases.py
+++ python/api/cases.py
@@ -1,34 +1,51 @@
+#---------------------------
+#everything about cases data
+#---------------------------
 from DBConnection import DBConnection
 import source
 import datetime
 
+# API method to read all cases
 def read_all():
     return Cases().loadAll();
+
+# API method to add a cases
 def add(cases):
     return Cases().add(cases);
+
+# API method receive cases by a filter request
 def search(cases):
     return Cases().search(cases);
 
+#---------------------------
+# Cases class handles all logic about cases
+#---------------------------
 class Cases(object):
+    
+    # reads all cases from DB
     def loadAll(self):
         statisticDB = DBConnection.getStatisticDB()
         return self.readCursor(statisticDB["cases"].find({}))
     
+    # query filtered cases from DB
     def search(self, cases):
         statisticDB = DBConnection.getStatisticDB()
         return self.readCursor(statisticDB["cases"].find(cases))
     
+    # add a new cases to the DB
     def add(self, cases):
         statisticDB = DBConnection.getStatisticDB()
         statisticDB["cases"].insert(self.apiToDB(cases));
     
+    
+    # converts the DB result to a list of cases
     def readCursor(self, cursor):
         casesList = list()
         for cases in cursor:
             casesList.append(self.dBToApi(cases))
         return casesList
     
-    
+    # cleans and converts the received data for the DB
     def apiToDB(self, cases):
         if ("source" in cases) :
             cases["source"] = source.getSource({"name": cases["source"]})
@@ -45,6 +62,7 @@ class Cases(object):
                     pass
         return cases
      
+     # converts the DB result for the API to be serializable
     def dBToApi(self, cases):
         if (cases is None) :
             return None

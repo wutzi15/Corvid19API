75ae2ec23fdb1065ffbb5591eb1faaf88695603a
Jan-Erik Matthies <jem@jemtech.de>
2020-03-23 10:05:58 +0100

    docu and bug fix

--- python/api/population.py
+++ python/api/population.py
@@ -1,20 +1,33 @@
+#---------------------------
+#everything about population data
+#---------------------------
 from DBConnection import DBConnection
+import misc
 
+# API method to read all population
 def read_all():
     return Population().loadAll()
+
+# API method receive population by a filter request
 def search(population):
     return Population().search(population)
 
+#---------------------------
+# Population class handles all logic about population
+#---------------------------
 class Population(object):
+    
+    # reads all population from DB
     def loadAll(self):
         statisticDB = DBConnection.getStatisticDB()
         return self.readCursor(statisticDB["Population"].find({}))
     
-    def search(self, measures):
+    # query filtered population from DB
+    def search(self, population):
         statisticDB = DBConnection.getStatisticDB()
         return self.readCursor(statisticDB["Population"].find(population))
     
-    
+    # converts the DB result to a list of population
     def readCursor(self, cursor):
         populations = list()
         for population in cursor:
@@ -22,10 +35,11 @@ class Population(object):
             populations.append(population)
         return populations
     
+    # cleans and converts the received data for the DB
     def dBToApi(self, population):
         if (population is None) :
             return None
-        population["_id"] = str(population["_id"])
+        misc.escapeID(population)
         if ("source" in population) :
-            population["source"]["_id"] = str(population["source"]["_id"])
+            misc.escapeID(population["source"])
         return population

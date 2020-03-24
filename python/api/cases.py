138ad884243ce3ea2721f995695b631fd173d602
Benedikt Bergenthal <benedikt.bergenthal@imflow.me>
2020-03-24 11:39:14 +0100

    Added a /cases/many endpoint to the API that acceppts an array of cases
    Signed-off-by: Benedikt Bergenthal <benedikt.bergenthal@imflow.me>

--- python/api/cases.py
+++ python/api/cases.py
@@ -17,41 +17,50 @@ def add(cases):
 def search(cases):
     return Cases().search(cases);
 
+def addMany(cases):
+    return Cases().addMany(cases)
+
 #---------------------------
 # Cases class handles all logic about cases
 #---------------------------
 class Cases(object):
-    
+
     # reads all cases from DB
     def loadAll(self):
         statisticDB = DBConnection.getStatisticDB()
         return self.readCursor(statisticDB["cases"].find({}))
-    
+
     # query filtered cases from DB
     def search(self, cases):
         statisticDB = DBConnection.getStatisticDB()
         return self.readCursor(statisticDB["cases"].find(cases))
-    
+
     # add a new cases to the DB
     def add(self, cases):
         statisticDB = DBConnection.getStatisticDB()
         statisticDB["cases"].insert(self.apiToDB(cases));
-    
-    
+
+    def addMany(self, cases):
+        statisticDB = DBConnection.getStatisticDB()
+        casesToInsert = []
+        for case in cases['cases']:
+            casesToInsert.append(self.apiToDB(case))
+        statisticDB["cases"].insert_many(casesToInsert)
+
     # converts the DB result to a list of cases
     def readCursor(self, cursor):
         casesList = list()
         for cases in cursor:
             casesList.append(self.dBToApi(cases))
         return casesList
-    
+
     # cleans and converts the received data for the DB
     def apiToDB(self, cases):
         if ("source" in cases) :
             cases["source"] = source.getSource({"name": cases["source"]})
         misc.toDateTime(cases, "date")
         return cases
-     
+
     # converts the DB result for the API to be serializable
     def dBToApi(self, cases):
         if (cases is None) :
